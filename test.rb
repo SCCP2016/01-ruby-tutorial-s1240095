5dafc9728b7d101217e0fce9492dc1b8ed2e3028
s1240095 <s1240095@u-aizu.ac.jp>
2016-04-28 19:38:18 +0900

    First submit

---
+++ test.rb
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+if 1 < 2 then
+  puts "aaaaa"
+end
+
+if 1 == 2 then
+  puts "bbbbb"
+end
+
+if 1==2 then
+  puts "ccccc"
+else
+  puts "ddddd"
+end
+
+x = 10
+
+if x > 100 then
+  puts "x is 100 over"
+elsif x % 2 == 0 then
+  puts "x is even"
+else
+  puts "x is odd"
+end

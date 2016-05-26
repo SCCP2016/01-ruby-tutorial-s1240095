025e71cd655363ea20b4b43bd630008c54a56a55
s1240095 <s1240095@u-aizu.ac.jp>
2016-05-26 19:39:49 +0900

    Changed using case

--- func.rb
+++ func.rb
@@ -5,24 +5,30 @@ end
 
 def bmi_check(height, weight)
   cbmi=bmi(height, weight)
-  if cbmi < 16 then
+  puts cbmi
+  case cbmi.to_i
+  when 0..15
     '痩せすぎ'
-  elsif cbmi >= 16 && cbmi <17 then
+  when 16
     '痩せ'
-  elsif cbmi >= 17 && cbmi <18.5 then
-    '痩せ気味'
-  elsif cbmi >= 18.5 && cbmi <25 then
-    '普通体重'
-  elsif cbmi >= 25 && cbmi <30 then
+  when 17..24
+    if cbmi <18.5 then
+      '痩せ気味'
+    else
+      '普通体重'
+    end
+  when 25..29
     '過体重'
-  elsif cbmi >= 30 && cbmi <35 then
+  when 30..34
     '肥満(1度)'
-  elsif cbmi >= 35 && cbmi <40 then
+  when 35..39
     '肥満(2度)'
   else
     '肥満(3度)'
   end
 end
+
+
 
 puts bmi(177,52).round(3)
 puts bmi_check(177,52)
